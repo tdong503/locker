c9b05833aef51ef76db53958278fcb1bf9b6379d
abduwaly <arsay@foxmail.com>
2020-09-04 22:28:35 +0800

    When first locker has larger capacity than the second locker, store bags into first locker

--- src/test/java/com/tw/locker/SmartLockerRobotTests.java
+++ src/test/java/com/tw/locker/SmartLockerRobotTests.java
@@ -14,6 +14,18 @@ public class SmartLockerRobotTests {
     private final String testLockerId2 = "Test Locker Id 2";
 
 
+
+    @Test
+    void should_save_bag_in_first_locker_and_return_a_ticket_when_save_bag_given_first_locker_capacity_is_larger_than_second() {
+        InitManagedLockers(2, 1);
+        Integer bagId = 1;
+        Ticket actual = smartLockerRobot.saveBag(new Bag(bagId));
+
+        assertNotNull(actual);
+        assertEquals(bagId, actual.getBagId());
+        assertEquals(testLockerId1, actual.getLockerId());
+    }
+
     @Test
     void should_return_corresponding_bag_when_take_bag_given_a_valid_ticket_provided() {
         InitManagedLockers(2, 2);
