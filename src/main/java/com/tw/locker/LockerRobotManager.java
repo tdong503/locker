7dfbb9f31559dfe8303e7c91c32a87c42252e5e7
abduwaly <arsay@foxmail.com>
2020-09-08 16:39:04 +0800

    Bag saved by first robot when locker robot manager manage two robots and both have capacity

--- src/main/java/com/tw/locker/LockerRobotManager.java
+++ src/main/java/com/tw/locker/LockerRobotManager.java
@@ -8,16 +8,27 @@ import java.util.Optional;
 
 public class LockerRobotManager {
 
-    protected final List<Locker> lockers;
+    private final List<Locker> lockers;
+    private final List<LockerRobotBase> robots;
 
-    public LockerRobotManager(LinkedList<Locker> lockers) {
+    public LockerRobotManager(LinkedList<Locker> lockers, LinkedList<LockerRobotBase> robots) {
         this.lockers = lockers;
+        this.robots = robots;
     }
 
     public Ticket saveBag(Bag bag) {
-        Optional<Locker> availableLocker = lockers.stream().filter(Locker::hasStorage).findFirst();
-        if(availableLocker.isPresent()) {
-            return availableLocker.get().saveBag(bag);
+        if(this.lockers != null){
+            Optional<Locker> availableLocker = this.lockers.stream().filter(Locker::hasStorage).findFirst();
+            if(availableLocker.isPresent()) {
+                return availableLocker.get().saveBag(bag);
+            }
+        }
+
+        if(this.robots != null){
+            Optional<LockerRobotBase> availableRobots = this.robots.stream().filter(r -> r.getAvailableLocker().isPresent()).findFirst();
+            if(availableRobots.isPresent()){
+                return availableRobots.get().saveBag(bag);
+            }
         }
 
         throw new NoStorageException();
--- src/test/java/com/tw/locker/LockerRobotManagerTests.java
+++ src/test/java/com/tw/locker/LockerRobotManagerTests.java
@@ -43,11 +43,32 @@ public class LockerRobotManagerTests {
         assertThrows(NoStorageException.class, () -> lockerRobotManager.saveBag(new Bag(bagId)));
     }
 
+    @Test
+    void should_saved_by_first_robot_when_save_bag_given_manage_two_robots_and_both_have_capacity() {
+        LinkedList<LockerRobotBase> robots = new LinkedList<>();
+
+        LinkedList<Locker> lockers1 = new LinkedList<>();
+        lockers1.add(new Locker(testLockerId1, 1));
+        robots.add(new PrimaryLockerRobot(lockers1));
+
+        LinkedList<Locker> lockers2 = new LinkedList<>();
+        lockers2.add(new Locker(testLockerId2, 1));
+        robots.add(new SmartLockerRobot(lockers2));
+
+        this.lockerRobotManager = new LockerRobotManager(null, robots);
+
+        Integer bagId = 1;
+        Ticket actual = lockerRobotManager.saveBag(new Bag(bagId));
+
+        assertNotNull(actual);
+        assertEquals(bagId, actual.getBagId());
+        assertEquals(testLockerId1, actual.getLockerId());
+    }
 
     private void InitManagedLockers(int firstLockerCapacity , int secondLockerCapacity) {
         LinkedList<Locker> lockers = new LinkedList<>();
         lockers.add(new Locker(testLockerId1, firstLockerCapacity));
         lockers.add(new Locker(testLockerId2, secondLockerCapacity));
-        this.lockerRobotManager = new LockerRobotManager(lockers);
+        this.lockerRobotManager = new LockerRobotManager(lockers, null);
     }
 }
