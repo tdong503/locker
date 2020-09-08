0e8846696d02a0e0e6114f3201def716fde8db5c
abduwaly <arsay@foxmail.com>
2020-09-08 09:40:43 +0800

    Save bag in who has capacity if locker-robot-manager manage two lockers and only one has capacity

--- src/test/java/com/tw/locker/LockerRobotManagerTests.java
+++ src/test/java/com/tw/locker/LockerRobotManagerTests.java
@@ -23,6 +23,17 @@ public class LockerRobotManagerTests {
         assertEquals(testLockerId1, actual.getLockerId());
     }
 
+    @Test
+    void should_save_bag_in_first_locker_and_return_ticket_when_save_bag_given_manage_two_locker_and_second_has_capacity_but_first_dose_not() {
+        InitManagedLockers(0,1);
+        Integer bagId = 1;
+        Ticket actual = lockerRobotManager.saveBag(new Bag(bagId));
+
+        assertNotNull(actual);
+        assertEquals(bagId, actual.getBagId());
+        assertEquals(testLockerId2, actual.getLockerId());
+    }
+
     private void InitManagedLockers(int firstLockerCapacity , int secondLockerCapacity) {
         LinkedList<Locker> lockers = new LinkedList<>();
         lockers.add(new Locker(testLockerId1, firstLockerCapacity));
